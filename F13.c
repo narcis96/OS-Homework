131e13c95b2208e703d60d1a0d7360eb14cbbd16
Gemene Narcis <gemenenarcis@Gemenes-MacBook-Air.local>
2017-08-15 14:59:28 +0300

    formatt code

--- F13.c
+++ F13.c
@@ -1,31 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-void main(int argc, char **argv)
-{
-    if(argc < 3)
-    {
-        fprintf(stderr,"Not enough parameters\n");
-         exit(EXIT_FAILURE);
+void main(int argc, char * * argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Not enough parameters\n");
+        exit(EXIT_FAILURE);
     }
-	FILE *fp = fopen(argv[1],"r");
-    if (fp == NULL)
-    {
-	   fprintf(stderr,"fopen() failed in file %s at line # %d", __FILE__,__LINE__);
-	   exit(EXIT_FAILURE);
+    FILE * fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "fopen() failed in file %s at line # %d", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
     }
- 	char ch = *argv[2];
-	char parseCh;
-	int count = 0;
-	while (!feof(fp))
-	{
-		fscanf(fp,"%c",&parseCh);
-		if(ch == parseCh)
-			count++;
-	}
-  	fclose(fp);
-    fprintf(stdout,"%d\n",count);
+    char ch = * argv[2];
+    char parseCh;
+    int count = 0;
+    while (!feof(fp)) {
+        fscanf(fp, "%c", & parseCh);
+        if (ch == parseCh)
+            count++;
+    }
+    fclose(fp);
+    fprintf(stdout, "%d\n", count);
     exit(EXIT_SUCCESS);
 }
-
--- F2.c
+++ F2.c
@@ -1,41 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int isPrime(int x)
-{
-        int d;
-        for(d = 2; d < x; d++)
-                if(x%d == 0)
-                        return 0;
-	return 1;
+int isPrime(int x) {
+    int d;
+    for (d = 2; d < x; d++)
+        if (x % d == 0)
+            return 0;
+    return 1;
 }
 
-void main(int argc, char **argv)
-{
-	if(argc < 2)
-	{
-		fprintf(stderr,"Not enough parameters\n");
+void main(int argc, char * * argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Not enough parameters\n");
         exit(EXIT_FAILURE);
     }
-	int x = atoi(argv[1]);
-	if(isPrime(x))
-	{
-		fprintf(stdout,"%d is prime\n",x);
-	}
-	else
-	{
-		fprintf(stdout,"%d is not prime\n",x);
-	}
+    int x = atoi(argv[1]);
+    if (isPrime(x)) {
+        fprintf(stdout, "%d is prime\n", x);
+    } else {
+        fprintf(stdout, "%d is not prime\n", x);
+    }
     exit(EXIT_SUCCESS);
 }
-
-
-
-
-
-
-
-
-
-
-
